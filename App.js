3c49186e3d7a4e2928ca00f621257d257c5cef3a
Eduardo Aguilar <es123y123@gmail.com>
2023-11-03 14:30:04 -0500

    BuildD

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { NavigationContainer } from '@react-navigation/native';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
-import { CustomBottomTabBar } from './CustomBottomTabBar';
-import Orientation from 'react-native-orientation';
-import { useEffect } from 'react';
 import { BoletosRuta } from './screens/BoletosRuta';
 import { PantallaCambiante } from './screens/PantallaCambiante';
 
